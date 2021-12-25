107d93393789c45c7c990cff55472451fec56960
eatea <al-loco@mail.ru>
2021-12-25 19:57:25 +0300

    send PHp gRPC message

--- client_service/client.php
+++ client_service/client.php
@@ -1,4 +1,27 @@
 <?php
+
+//phpinfo(); exit();
+
 require "./vendor/autoload.php";
 
-$client = new \Grpc_service\GetRequest();
+use Grpc_service\GrpcServiceClient;
+
+$host = 'localhost:8080';
+
+$client = new GrpcServiceClient(
+    $host,
+    ['credentials' => Grpc\ChannelCredentials::createInsecure()]
+);
+
+$request = new \Grpc_service\GetRequest();
+
+/**
+ * @var $response \Grpc_service\GetResponse
+ */
+list($response, $status) = $client->GetSettings($request)->wait();
+
+if ($status->code !== Grpc\STATUS_OK) {
+    echo "ERROR: " . $status->code . ", " . $status->details . PHP_EOL;
+    exit(1);
+}
+var_dump($response->serializeToJsonString()) . PHP_EOL;
